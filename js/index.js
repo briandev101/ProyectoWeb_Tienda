99a0e77f0254fce88d12d6994bffa0132cf3a11c
briandev101 <brian.v.compras@gmail.com>
2025-06-23 11:40:30 +0200

    Style:60% correciones y mediaQuerty:incompleto

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+// plantilla de producto para la tienda
+
+/*
+<article class="producto">
+    <img src="ruta-de-la-imagen.jpg" alt="Nombre del producto" class="producto-imagen">
+    <h3 class="producto-titulo">Camiseta verde para niño manga corta</h3>
+    <p class="producto-precios">
+        <span class="precio-descuento">RD$1,450.00</span>
+        <button class="producto-boton">Comprar Ahora</button>
+    </p>
+</article>
+*/ 
+
